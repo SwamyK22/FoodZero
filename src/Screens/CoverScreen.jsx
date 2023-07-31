0bb623b6f90d9bcb6adc7b5cb826098edf10509b
AnithaGummalla <109032010+AnithaGummalla@users.noreply.github.com>
2023-07-31 14:29:04 +0530

    CoverScreen

---
+++ src/Screens/CoverScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CoverScreen = () => {
+  return (
+    <div>CoverScreen
+        <img src="https://www.figma.com/file/pscfpZCEVlLpwJu8sfg1Vi/FoodZero-Restaurant-and-Food-for-Figma-Web-Template-(Community)?type=design&node-id=3577-27&mode=design&t=aXcflPD8JLddwu4a-4" alt="" />
+    </div>
+  )
+}
+
+export default CoverScreen
